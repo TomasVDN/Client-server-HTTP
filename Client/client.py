5eb939e7a7dfd329ad3e52b8f3e6525af1c0e2f8
TomasVDN <tomas.vandennoortgate@student.kuleuven.be>
2021-03-28 08:47:49 +0200

    comments done

--- Client/client.py
+++ Client/client.py
@@ -5,39 +5,45 @@ import shutil
 import socket
 import sys
 
+# Import BeautifulSoup, used to find the images.
 from bs4 import BeautifulSoup
 
+##############################
 
+# Class used to process the request. Parses the input arguments (argv), and compiles the correct message to send to the
+# web servers
 class Request:
-    def __init__(self, list):
+
+    # In the init, process the input arguments. Expects at least a method and an URL. If no port is given, default to 80
+    def __init__(self, list_of_input_element):
 
         # Method
-        self.method = list[0]
+        self.method = list_of_input_element[0]
 
-        # Host & resource
-        pos = list[1].find("/")
+        # Host & resource. If no resource is given, set it to /.
+        pos = list_of_input_element[1].find("/")
         if pos == -1:
-            self.host = list[1]
+            self.host = list_of_input_element[1]
             self.resource = "/"
         else:
-            self.host = list[1][0:pos]
-            self.resource = list[1][pos:]
+            self.host = list_of_input_element[1][0:pos]
+            self.resource = list_of_input_element[1][pos:]
 
         # Port
-        if len(list) == 3:
-            self.port = list[2]
+        if len(list_of_input_element) == 3:
+            self.port = list_of_input_element[2]
         else:
             self.port = 80
 
         self.make_request()
 
-    # Compiles the request
+    # Compiles the method, host and resource into a valid HTTP request.
     def make_request(self):
         self.request = '{} {} HTTP/1.1\r\nHost: {}\r\n'.format(self.method, self.resource, self.host).encode()
 
         request_body = None
 
-        # Appends Content type and length to the header
+        # Appends Content type and length to the header & reads the body.
         if self.method == "PUT" or self.method == "POST":
 
             file = input("Please, enter a file to send:\n")
@@ -54,7 +60,7 @@ class Request:
             self.request += 'Content-Type: {}\r\nContent-Length: {}\r\n'.format(content_type, len(request_body)).encode()
 
         # Appends the asked file to the body (if there is one)
-        if not request_body is None:
+        if request_body:
             self.request += '\r\n'.encode() + request_body
 
         # End of request
@@ -66,17 +72,24 @@ class Request:
         self.resource = resource
         self.make_request()
 
-#########################################
+##############################
+
+
+# Remove the data present on the socket
 def empty_socket(sock):
-    """remove the data present on the socket"""
     input = [sock]
     while 1:
-        inputready, o, e = select.select(input,[],[], 0.0)
-        if len(inputready)==0: break
-        for s in inputready: s.recv(1)
+        input_ready, o, e = select.select(input, [], [], 0.0)
+        if len(input_ready) == 0:
+            break
+        for s in input_ready: s.recv(1)
 
 
+# Class used to receive and process the response.
 class Response:
+
+    # Function to initiate all defaults and read the header. When header has been read, call the correct body receiver
+    # method (chunked or with content length)
     def __init__(self, connection):
         self.body = b""
         self.code = None
@@ -84,15 +97,18 @@ class Response:
         self.content_length = None
         self.is_chunked = False
 
+        # Read and process header.
         self.header = self.receive_header(connection)
+
+        # Call correct body method reader. If no body is expected, no one is called.
         if self.is_chunked:
             self.receive_chunked_body(connection)
         elif self.content_length is not None:
             self.receive_content_length_body(connection)
 
+    # Function to receive header, and process interesting information.
     def receive_header(self, connection):
         data = bytes()
-        chunk = bytes()
 
         # Receive header, and decode
         while b'\r\n\r\n' not in data:
@@ -102,10 +118,10 @@ class Response:
 
         data = data.decode()
 
-        # Get return code
+        # Get status code
         self.code= data.split("\r\n")[0].split(" ")[1]
 
-        # Change header to a dict
+        # Change header elements to a dict
         header_elements = {k: v.strip() for k, v in
                            [line.split(":", 1) for line in data.splitlines() if ":" in line]}
 
@@ -123,6 +139,7 @@ class Response:
 
         return data
 
+    # Function to receive chunked bodies.
     def receive_chunked_body(self, connection):
         while True:
             # Get length of the next chunk
@@ -137,6 +154,7 @@ class Response:
                 return
             remaining = length + 2  # +2 for \r\n
 
+            # Read chunk
             chunk = b''
             while remaining > 0:
                 if remaining > 1024:
@@ -149,6 +167,8 @@ class Response:
                 
             self.body += chunk
 
+    # Function to receive a body with content_length. Receives in chunks of 1024 by default, until there is less than
+    # 1024 bytes of data left.
     def receive_content_length_body(self, connection, chunk_size = 1024):
         data = bytes()
 
@@ -164,14 +184,18 @@ class Response:
         self.body = data
         empty_socket(connection)
 
-###########################
+##############################
 
 
+# Function to import the images from a HTML file. Uses BeautifulSoup to parse the HTML. The folder structure remains almost
+# the same as on the server.
 def import_images(connection, request, response):
     soup = BeautifulSoup(response.body.decode(response.encoding), "html.parser")
 
+    # Find all images
     images = soup.findAll('img')
 
+    # Send a GET request to get all images.
     for image in images:
         src = image['src']
 
@@ -188,19 +212,19 @@ def import_images(connection, request, response):
         response = Response(connection)
         print("[RECEIVED]\n" + response.header)
 
-        dir = os.path.dirname(__file__)
-
-        directory = dir + "/output" + os.path.dirname(src)
+        # Check if directories exists. If not, create it.
+        directory = "output" + os.path.dirname(src)
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        with open(dir + "/output/" + src, "wb") as out:
+        with open("output/" + src, "wb") as out:
             out.write(response.body)
             out.close()
 
         if src[0] == "/": src = src[1:]
         image['src'] = src
 
+    # Do the same as above, but with lowsrc.
     for image in images:
         if image.has_attr("lowsrc"):
             src = image['lowsrc']
@@ -218,25 +242,24 @@ def import_images(connection, request, response):
             response = Response(connection)
             print("[RECEIVED]\n" + response.header)
 
-            dir = os.path.dirname(__file__)
-
-            directory = dir + "/output" + os.path.dirname(src)
+            directory = "output" + os.path.dirname(src)
             if not os.path.exists(directory):
                 os.makedirs(directory)
 
-            with open(dir + "/output/" + src, "wb") as out:
+            # Write to file
+            with open("output/" + src, "wb") as out:
                 out.write(response.body)
                 out.close()
 
             if src[0] == "/": src = src[1:]
             image['src'] = src
 
-    dir = os.path.dirname(__file__)
-    with open(dir + "/output/output.html", "w") as out:
+    # Write html to output/output.html
+    with open("output/output.html", "w") as out:
         out.write(str(soup))
         out.close()
 
-#################
+##############################
 
 
 def main(list):
@@ -248,6 +271,7 @@ def main(list):
             elif entry.is_dir():
                 shutil.rmtree(entry.path)
 
+    # Make a request
     request = Request(list)
 
     # Initiate socket connection
@@ -263,6 +287,7 @@ def main(list):
     response = Response(sock)
     print("[RECEIVED]\n" + response.header)
 
+    # If method is GET and everything happened as intended, get images.
     if request.method == "GET" and int(response.code) == 200:
         import_images(sock, request, response)
 
--- Server/server.py
+++ Server/server.py
@@ -4,13 +4,15 @@ import select
 import socket
 import sys
 import threading
-
-#TODO timestamp
-
-from email.utils import formatdate
+from datetime import datetime
+import time
 from urllib.parse import urlparse
 
 
+##############################
+
+
+# Remove the data present on the socket
 def empty_socket(sock):
     input = [sock]
     while 1:
@@ -19,7 +21,10 @@ def empty_socket(sock):
         for s in inputready: s.recv(1)
 
 
+# Class used to receive and separate the header from the body.
 class Separator:
+    # Function to initiate all defaults and read the header. When header has been read, call the correct body receiver
+    # method (chunked or with content length)
     def __init__(self, connection):
         self.body = b""
         self.code = None
@@ -27,12 +32,16 @@ class Separator:
         self.content_length = None
         self.is_chunked = False
 
+        # Read and process header.
         self.header = self.receive_header(connection)
+
+        # Call correct body method reader. If no body is expected, no one is called.
         if self.is_chunked:
             self.receive_chunked_body(connection)
         elif self.content_length is not None:
             self.receive_content_length_body(connection)
 
+    # Function to receive header, and process interesting information.
     def receive_header(self, connection):
         data = bytes()
         chunk = bytes()
@@ -44,9 +53,6 @@ class Separator:
             data += chunk
 
         data = data.decode()
-
-        # Get return code
-        self.code = data.split("\r\n")[0].split(" ")[1]
 
         # Change header to a dict
         header_elements = {k: v.strip() for k, v in
@@ -61,6 +67,7 @@ class Separator:
 
         return data.encode()
 
+    # Function to receive chunked bodies.
     def receive_chunked_body(self, connection):
         while True:
             # Get length of the next chunk
@@ -87,6 +94,8 @@ class Separator:
 
             self.body += chunk
 
+    # Function to receive a body with content_length. Receives in chunks of 1024 by default, until there is less than
+    # 1024 bytes of data left.
     def receive_content_length_body(self, connection, chunk_size=1024):
         data = bytes()
 
@@ -102,35 +111,50 @@ class Separator:
         self.body = data
         empty_socket(connection)
 
+
+##############################
+
+
+# Class used to process the incoming request. Parses the header to extract all the useful information.
 class Request:
+    # Function to initiate all defaults and read the header
     def __init__(self, header, body):
         self.method = None
         self.uri = None
-        self.http_version = "1.1" # default to HTTP/1.1
-        self.keep_connection = True # default to True (HTTP/1.1)
+        self.http_version = "HTTP/1.1"
+        self.keep_connection = True  # default to True (HTTP/1.1)
         self.encoding = "iso-8859-1"
         self.is_host_present = False
-        self.if_modified_since = False
-        self.date = None
+        self.if_modified_since = None
+        self.if_unmodified_since = None
 
-        self.header = self.parse(header)
+        self.header = header
+        self.parse(header)
         self.body = body
 
+    # Parses the header, to find all the useful headers.
     def parse(self, data):
         lines = data.split(b"\r\n")
         request_line = lines[0]
         words = request_line.split(b" ")
 
+        # Method
         self.method = words[0].decode()
-        self.uri = words[1].decode() #uri
-        if "http" in self.uri: # absolute url
+
+        # Resource
+        self.uri = words[1].decode()  # uri
+        if "http" in self.uri:  # absolute url
             parsed = urlparse(self.uri)
             self.uri = parsed.path
 
-        self.http_version = words[2]
+        # HTTP version
+        self.http_version = words[2].decode()
 
-        header_elements = { k:v.strip() for k,v in [line.split(":",1) for line in data.decode().splitlines() if ":" in line]}
+        # Change header elements to a dict
+        header_elements = {k: v.strip() for k, v in
+                           [line.split(":", 1) for line in data.decode().splitlines() if ":" in line]}
 
+        # Check all useful header fields
         for key in header_elements:
             if key == "Host":
                 self.is_host_present = True
@@ -142,19 +166,24 @@ class Request:
                     self.encoding = "utf-8"
                 elif "iso-8859-1" in header_elements[key]:
                     self.encoding = "iso-8859-1"
+            elif key == "If-Modified-Since":
+                self.if_modified_since = datetime.strptime(header_elements[key], "%a, %d %b %Y %I:%M:%S GMT")
+            elif key == "If-Unmodified-Since":
+                self.if_unmodified_since = datetime.strptime(header_elements[key], "%a, %d %b %Y %I:%M:%S GMT")
 
 
-def end_removal(value, a):
-    pos_a = value.rfind(a)
-    if pos_a == -1: return ""
-    return value[0:pos_a]
+##############################
 
+
+# Main class. This server checks a port for incoming connections. If there is one, makes a serverThread to
+# process further interactions
 class Server:
     def __init__(self, host='127.0.0.1', port=9000):
         self.host = host
         self.port = port
 
     def start(self):
+        # Make connection, and listen to port.
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind((self.host, self.port))
@@ -162,6 +191,7 @@ class Server:
 
         print("Listening at", sock.getsockname())
 
+        # While True, listen to port, and make thread there is an incoming connection.
         while True:
             conn, address = sock.accept()
             print("Connection initiated with:", address)
@@ -169,6 +199,11 @@ class Server:
             thread.start()
 
 
+##############################
+
+
+# ServerThread class. This class defines everything needed to further handle the connection with a client.
+# It processes all the information.
 class ServerThread(threading.Thread):
     def __init__(self, connection, address):
         threading.Thread.__init__(self)
@@ -177,124 +212,189 @@ class ServerThread(threading.Thread):
 
     def run(self):
         keep_connection = True
-        while (keep_connection):
+
+        # Eternal loop, until client closes or asks to close the connection.
+        while keep_connection:
+
+            # Try receiving. If an error occurs, it means the connection has been closed on the client side.
+            # In that case, break while loop.
             try:
                 sep = Separator(self.connection)
             except Exception:
                 break
 
+            # Process request.
             request = Request(sep.header, sep.body)
 
+            # Get the correct handler for the given method. Also checks for errors (HTTP version, Host,...).
             try:
-                if not request.is_host_present:
+                if not request.http_version == "HTTP/1.1":
+                    handler = self.handle_505
+                elif not request.is_host_present:
                     handler = self.handle_400
                 else:
                     handler = getattr(self, 'handle_%s' % request.method)
             except AttributeError:
                 handler = self.handle_501
 
+            # Make response
             response = handler(request)
 
+            # Send response
             self.connection.send(response)
 
+            # If connection: close header has been sent, close the connection.
             if not request.keep_connection:
                 self.connection.close()
                 print("Closed connection with:", self.address)
                 sys.exit()
 
+        # If while loop has been broken, client has closed his side of the connection. In that case, close
+        # our side.
         self.connection.close()
         print("Closed connection with:", self.address)
         sys.exit()
 
+    # default headers.
     headers = {
         'Server': 'Localhost',
-        'Content-Type': 'text/html',
-        'Content-Length': '0',
     }
 
     status_codes = {
-        200: 'OK',
-        201: 'Created',
-        304: 'Not Modified',
-        400: 'Bad Request',
-        404: 'Not Found',
-        501: 'Not Implemented',
+        200: 'OK',  # Standard successful response
+        201: 'Created',  # Response if file has been created
+        304: 'Not Modified',  # Response if file has not been modified (GET,HEAD, If-Modified-Since)
+        400: 'Bad Request',  # Host not present
+        404: 'Not Found',  # File not found
+        412: 'Precondition Failed',  # Response if file has been modified (PUT, POST, If-Unmodified-Since)
+        501: 'Not Implemented',  # Response for unimplemented methods (DELETE, OPTIONS,...)
+        505: 'HTTP Version Not Supported',  # Response for unsupported HTTP versions
     }
 
+    # Make a response line.
     def response_line(self, status_code):
-        """Returns response line"""
         reason = self.status_codes[status_code]
         line = "HTTP/1.1 %s %s\r\n" % (status_code, reason)
 
-        return line.encode()  # call encode to convert str to bytes
+        return line.encode()
 
+    # Process the headers.
     def response_headers(self, extra_headers=None):
-        headers_copy = self.headers.copy()  # make a local copy of headers
+        # make a local copy of headers
+        headers_copy = self.headers.copy()
 
+        # Merge extra headers and default headers
         if extra_headers:
-            headers_copy.update(extra_headers)
+            headers_copy = {**headers_copy, **extra_headers}
 
         headers = ""
 
         for h in headers_copy:
-            headers += "%s: %s\r\n" % (h, headers_copy[h])
+            headers += "{}: {}\r\n".format(h, headers_copy[h])
 
-        headers += "Date: %s\r\n" % (formatdate(timeval=None, localtime=False, usegmt=True))
+        # Add Date to headers.
+        headers += "Date: " + time.strftime("%a, %d %b %Y %I:%M:%S", time.gmtime()) + " GMT\r\n"
 
-        return headers.encode()  # call encode to convert str to bytes
+        return headers.encode()
 
+    # 501 handler (Not implemented).
     def handle_501(self, request):
-            response_line = self.response_line(status_code=501)
+        response_line = self.response_line(status_code=501)
 
-            blank_line = b"\r\n"
+        blank_line = b"\r\n"
 
-            response_body = b"<h1>501 Not implemented</h1>"
+        response_body = b"<h1>501 Not implemented</h1>"
 
-            extra_headers = {'Content-Length': len(response_body)}
-            response_headers = self.response_headers(extra_headers)
+        extra_headers = {'Content-Type': 'text/html', 'Content-Length': len(response_body)}
+        response_headers = self.response_headers(extra_headers)
 
-            return b"".join([response_line, response_headers, blank_line, response_body, blank_line, blank_line])
+        return b"".join([response_line, response_headers, blank_line, response_body, blank_line, blank_line])
 
+    # 505 handler (Not supported HTTP version).
+    def handle_505(self, request):
+        response_line = self.response_line(status_code=505)
+
+        blank_line = b"\r\n"
+
+        response_body = b"<h1>HTTP Version Not Supported</h1>"
+
+        extra_headers = {'Content-Type': 'text/html', 'Content-Length': len(response_body)}
+        response_headers = self.response_headers(extra_headers)
+
+        return b"".join([response_line, response_headers, blank_line, response_body, blank_line, blank_line])
+
+    # 400 handler (Bad request (called when Host is absent)).
     def handle_400(self, request):
-            response_line = self.response_line(status_code=400)
+        response_line = self.response_line(status_code=400)
 
-            blank_line = b"\r\n"
+        blank_line = b"\r\n"
 
-            response_body = b"<h1>400 Bad request</h1>"
+        response_body = b"<h1>400 Bad request</h1>"
 
-            extra_headers = {'Content-Length': len(response_body)}
-            response_headers = self.response_headers(extra_headers)
+        extra_headers = {'Content-Type': 'text/html', 'Content-Length': len(response_body)}
+        response_headers = self.response_headers(extra_headers)
 
-            return b"".join([response_line, response_headers, blank_line, response_body, blank_line, blank_line])
+        return b"".join([response_line, response_headers, blank_line, response_body, blank_line, blank_line])
 
+    # GET handler.
     def handle_GET(self, request):
         filename = request.uri.strip('/')  # remove the slash from the request URI
 
+        # Check if file exists
         if os.path.exists(filename):
+            dt = os.path.getmtime(filename)
+            modification_time = datetime.fromtimestamp(dt)
+
+            # Check modification date for the If-Modified-Since header
+            if request.if_modified_since:
+                if modification_time < request.if_modified_since:
+                    response_line = self.response_line(status_code=304)
+                    blank_line = b"\r\n"
+
+                    response_headers = self.response_headers()
+                    return b"".join([response_line, response_headers, blank_line, blank_line])
+
             response_line = self.response_line(status_code=200)
 
             # find out a file's MIME type
             # if nothing is found, just send `text/html`
             content_type = mimetypes.guess_type(filename)[0] or 'text/html'
 
+            # Read file to body.
             with open(filename, 'rb') as f:
                 response_body = f.read()
         else:
+            # If file not found
             response_line = self.response_line(status_code=404)
             content_type = 'text/html'
             response_body = b"<h1>404 Not Found</h1>"
 
         blank_line = b"\r\n"
 
+        # Make extra headers, and make header.
         extra_headers = {'Content-Type': content_type, 'Content-Length': len(response_body)}
         response_headers = self.response_headers(extra_headers)
 
         return b"".join([response_line, response_headers, blank_line, response_body, blank_line, blank_line])
 
+    # HEAD handler
     def handle_HEAD(self, request):
         filename = request.uri.strip('/')  # remove the slash from the request URI
 
+        # Check if file exists
         if os.path.exists(filename):
+            dt = os.path.getmtime(filename)
+            modification_time = datetime.fromtimestamp(dt)
+
+            # Check modification date for the If-Modified-Since header
+            if request.if_modified_since:
+                if modification_time < request.if_modified_since:
+                    response_line = self.response_line(status_code=304)
+                    blank_line = b"\r\n"
+
+                    response_headers = self.response_headers()
+                    return b"".join([response_line, response_headers, blank_line, blank_line])
+
             response_line = self.response_line(status_code=200)
 
             # find out a file's MIME type
@@ -305,6 +405,7 @@ class ServerThread(threading.Thread):
             response_headers = self.response_headers(extra_headers)
 
         else:
+            # If file not found
             response_line = self.response_line(status_code=404)
             response_headers = self.response_headers()
 
@@ -312,14 +413,31 @@ class ServerThread(threading.Thread):
 
         return b"".join([response_line, response_headers, blank_line, blank_line])
 
+    # PUT handler
     def handle_PUT(self, request):
         return_path = "documents" + request.uri
         filename = "documents" + request.uri
 
+        # Checks if file exists.
+        if os.path.exists(filename):
+            dt = os.path.getmtime(filename)
+            modification_time = datetime.fromtimestamp(dt)
+
+            # Check modification date for the If-Unmodified-Since header
+            if request.if_unmodified_since:
+                if modification_time > request.if_unmodified_since:
+                    response_line = self.response_line(status_code=412)
+                    blank_line = b"\r\n"
+
+                    response_headers = self.response_headers()
+                    return b"".join([response_line, response_headers, blank_line, blank_line])
+
+        # Make directories if they do not exist.
         directory = os.path.dirname(filename)
         if not os.path.exists(directory):
             os.makedirs(directory)
 
+        # (Over)Write file
         with open(filename, 'w') as out:
             print(request.body.decode())
             out.write(request.body.decode(request.encoding))
@@ -334,10 +452,26 @@ class ServerThread(threading.Thread):
 
         return b"".join([response_line, response_headers, blank_line, blank_line])
 
+    # POST handler
     def handle_POST(self, request):
         return_path = "documents" + request.uri
         filename = "documents" + request.uri
 
+        # Checks if file exists.
+        if os.path.exists(filename):
+            dt = os.path.getmtime(filename)
+            modification_time = datetime.fromtimestamp(dt)
+
+            # Check modification date for the If-Unmodified-Since header
+            if request.if_unmodified_since:
+                if modification_time > request.if_unmodified_since:
+                    response_line = self.response_line(status_code=412)
+                    blank_line = b"\r\n"
+
+                    response_headers = self.response_headers()
+                    return b"".join([response_line, response_headers, blank_line, blank_line])
+
+        # Check if file already exists. If yes, append. Otherwise, create new one.
         if os.path.isfile(filename):
             with open(filename, 'a') as out:
                 out.write(request.body.decode(request.encoding))
@@ -360,6 +494,9 @@ class ServerThread(threading.Thread):
         response_headers = self.response_headers(extra_headers)
 
         return b"".join([response_line, response_headers, blank_line, blank_line])
+
+##############################
+
 
 if __name__ == '__main__':
     server = Server()
